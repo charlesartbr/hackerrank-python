ea96e1c76e5f7d091efccd81c1c7bef4f6a9d344
Charles Cavalcante <e-mail@charles.art.br>
2019-06-18 12:23:40 -0300

    correction in input

--- strings/swap-case/python3.py
+++ strings/swap-case/python3.py
@@ -3,3 +3,6 @@ def swap_case(s):
     return ''.join([c.lower() if c.isupper() else c.upper() for c in s])
 
 if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print (result)
