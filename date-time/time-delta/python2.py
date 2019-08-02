176aa6e32aeca74467911f26cfe87df8d93736bd
Charles Cavalcante <e-mail@charles.art.br>
2019-08-02 19:20:20 -0300

    Time Delta: solution

--- date-time/time-delta/python2.py
+++ date-time/time-delta/python2.py
@@ -13,10 +13,22 @@ def time_delta(t1, t2):
     d1 = date.datetime.strptime(t1[4:24], '%d %b %Y %H:%M:%S')
     d2 = date.datetime.strptime(t2[4:24], '%d %b %Y %H:%M:%S')
 
-    s1 = (int(t1[26:28]) * 60 * 60) + (int(t1[29]) * 60)
-    s2 = (int(t2[26:28]) * 60 * 60) + (int(t2[29]) * 60)
+    h1 = date.timedelta(hours=int(t1[26:28]), minutes=int(t1[28:]))
+    h2 = date.timedelta(hours=int(t2[26:28]), minutes=int(t2[28:]))
+
+    if t1[25] == '-':
+        d1 += h1
+    else:
+        d1 -= h1
     
-    return str(h1) + ' x ' + str(h2)
+    if t2[25] == '-':
+        d2 += h2
+    else:
+        d2 -= h2
+
+    d = d1 - d2
+
+    return str(int(abs(d.total_seconds())))
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -24,8 +36,8 @@ if __name__ == '__main__':
     t = int(raw_input())
 
     for t_itr in xrange(t):
-        t1 = raw_input()
 
+        t1 = raw_input()
         t2 = raw_input()
 
         delta = time_delta(t1, t2)
--- date-time/time-delta/python3.py
+++ date-time/time-delta/python3.py
@@ -1,5 +1,34 @@
-import calendar
+#!/bin/python3
 
-m, d, y = map(int, input().split())
+import os
+from datetime import datetime, timedelta
 
-print(calendar.day_name[calendar.weekday(y, m, d)].upper())
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+
+    d1 = datetime.strptime(t1[4:24], '%d %b %Y %H:%M:%S')
+    d2 = datetime.strptime(t2[4:24], '%d %b %Y %H:%M:%S')
+
+    h1 = timedelta(hours=int(t1[26:28]), minutes=int(t1[28:]))
+    h2 = timedelta(hours=int(t2[26:28]), minutes=int(t2[28:]))
+
+    d1 = d1 + h1 if t1[25] == '-' else d1 - h1
+    d2 = d2 + h2 if t2[25] == '-' else d2 - h2
+
+    return str(int(abs((d1 - d2).total_seconds())))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+
+        t1 = input()
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(delta + '\n')
+
+    fptr.close()
