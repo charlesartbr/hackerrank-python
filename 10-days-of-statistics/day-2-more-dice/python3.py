4a62961826fb9fbc889e57db6992ed45b02239f3
Charles Cavalcante <e-mail@charles.art.br>
2019-08-19 11:21:14 -0300

    better readability

--- 10-days-of-statistics/day-2-more-dice/python3.py
+++ 10-days-of-statistics/day-2-more-dice/python3.py
@@ -3,6 +3,6 @@ from fractions import Fraction
 
 p = list(product([1,2,3,4,5,6], repeat=2))
 
-n = sum(1 for x in p if sum(x) == 6 and x[0] != x[1])
+n = sum(1 for x, y in p if x + y == 6 and x != y)
 
 print(Fraction(n, len(p)))
