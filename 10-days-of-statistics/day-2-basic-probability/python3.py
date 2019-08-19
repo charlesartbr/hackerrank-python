ed2d78ef02b705c398fa1ce7d388d7c0719f8f8a
Charles Cavalcante <e-mail@charles.art.br>
2019-08-19 11:22:18 -0300

    added Fraction

--- 10-days-of-statistics/day-2-basic-probability/python3.py
+++ 10-days-of-statistics/day-2-basic-probability/python3.py
@@ -3,8 +3,6 @@ from fractions import Fraction
 
 p = list(product([1,2,3,4,5,6], repeat=2))
 
-c = len(p)
-
 n = sum(1 for x in p if sum(x) <= 9)
 
-print("%d / 6" % (n / c * 6))
+print(Fraction(n, len(p)))
