cfcfb2fc2e6ea99d6103ade4d3c46e87175b5ae7
Charles Cavalcante <e-mail@charles.art.br>
2019-10-04 10:54:37 -0300

    Day 22: Binary Search Trees: solution

--- 30-days-of-code/day-22-binary-search-trees/python3.py
+++ 30-days-of-code/day-22-binary-search-trees/python3.py
@@ -1,24 +1,38 @@
-n = int(input().strip())
-a = list(map(int, input().strip().split(' ')))
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
 
-total_swaps = 0
+    def getHeight(self,root):
+        
+        left = 0
+        right = 0
+        
+        if not root.right is None:
+            right = 1 + self.getHeight(root.right)
+        
+        if not root.left is None:
+            left = 1 + self.getHeight(root.left)
 
-for i in range(n):
+        return max([left, right])
 
-    swaps = 0
-
-    for j in range(n-1):
-
-        if a[j] > a[j+1]:
-            temp = a[j]
-            a[j] = a[j+1]
-            a[j+1] = temp
-            swaps += 1
-            total_swaps += 1
-
-    if swaps == 0:
-        break
-
-print('Array is sorted in %d swaps.' % total_swaps)
-print('First Element:', a[0])
-print('Last Element:', a[n-1])
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)       
