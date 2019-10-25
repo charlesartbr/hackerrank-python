5b854b9d0c9a97f8371509ce852a84bb0396bb53
Charles Cavalcante <e-mail@charles.art.br>
2019-10-25 11:09:14 -0300

    better performance

--- interview-preparation-kit/arrays/left-rotation/python.py
+++ interview-preparation-kit/arrays/left-rotation/python.py
@@ -1,13 +1,4 @@
-def rotLeft(a, d):
-
-    for i in range(d):
-        a = a[1:] + [a[0]]
-
-    return a
-
 n, d = map(int, input().split())
 a = list(map(int, input().rstrip().split()))
 
-result = rotLeft(a, d)
-
-print(' '.join(map(str, result)))
+print(' '.join(map(str, a[d:] + a[:d])))
