28add49da1099ca659dc27bf74a292cd592c1fb8
Charles Cavalcante <e-mail@charles.art.br>
2020-08-09 09:55:13 -0300

    Count Triplets - 8/13 (#27)

--- interview-preparation-kit/dictionaries-hashmaps/count-triplets/count-triplets.py
+++ interview-preparation-kit/dictionaries-hashmaps/count-triplets/count-triplets.py
@@ -1,28 +1,26 @@
+from math import factorial
+
 def countTriplets(arr, r):
 
     triplets = 0
-    num_counter = {}
+    triplets_dict = {}
 
-    for i in range(len(arr)):
+    for i in reversed(arr):
 
-        n = arr[i]
-
-        if n in num_counter:
-            num_counter[n] += 1
+        if i in triplets_dict:
+            triplets_dict[i] += 1
         else:
-            num_counter[n] = 1
+            triplets_dict[i] = 1
 
-    for n1 in num_counter:
-        
-        n2 = n1 * r
-        n3 = n2 * r
+        r1 = i * r
 
-        if n2 in num_counter and n3 in num_counter:
+        if r1 in triplets_dict:
 
-            if n1 == n2 and n1 == n3:
-                triplets += (num_counter[n1] * (num_counter[n1]-1) * (num_counter[n3]-2)) // 6
-            else:
-                triplets += num_counter[n1] * num_counter[n2] * num_counter[n3]
+            r2 = i * r * r
+
+            if r2 in triplets_dict:
+
+                triplets += triplets_dict[r1] * triplets_dict[r2]
 
     return triplets
 
